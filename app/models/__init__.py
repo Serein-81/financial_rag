2e184e896f304a9999791a34f25661df68a5034a
Serein-81 <chen_jh81@qq.com>
2026-03-13 18:25:23 +0800

    feat: 扩展用户模型
    - 添加手机号和头像字段
    - 支持多种登录方式
    - 更新模型导出"

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,2 +1,11 @@
+from .user import User
+from .knowledge_base import KnowledgeBase
+from .chat import ChatSession
 from .document import Document
 from .chunk import DocumentChunk
+from .search_log import SearchLog
+from .system_log import SystemLog, UserActionLog
+from .agent_trace import AgentTrace, AgentStep
+from .tool_trace import ToolCallTrace
+from .prompt_optimization import PromptTemplate, PromptExecution, PromptABTest
+from .semantic_memory import SemanticMemory
